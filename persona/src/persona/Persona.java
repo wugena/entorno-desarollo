7a932743a7f712233e27b697e4019d170a817d86
wugena <162334776+wugena@users.noreply.github.com>
2024-03-14 16:07:58 +0100

    proyecto persona creado

---
+++ persona/src/persona/Persona.java
@@ -0,0 +1,113 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String Dni;
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * 
+     */
+    private String Apellido;
+
+    /**
+     * 
+     */
+    private int Edad;
+
+    /**
+     * @param dni 
+     * @param name  
+     * @param Apellido 
+     * @param Edad
+     */
+    public void Persona(String dni, String name , String Apellido, int Edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica DNI de la personas
+     * @param Dni
+     */
+    public void setDni(String Dni) {
+        // TODO implement here
+    }
+
+    /**
+     * devolver DNI de la personas
+     * @return
+     */
+    public String GetDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica DNI de la personas
+     * @param Name
+     */
+    public void SetName(String Name) {
+        // TODO implement here
+    }
+
+    /**
+     * devolver DNI de la personas
+     * @return
+     */
+    public String GetName() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica DNI de la personas
+     * @param Apellido
+     */
+    public void SetApellido(String Apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * devolver DNI de la personas
+     * @return
+     */
+    public String GetApellido() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica DNI de la personas
+     * @param Edad
+     */
+    public void SetEdad(int Edad) {
+        // TODO implement here
+    }
+
+    /**
+     * devolver DNI de la personas
+     * @return
+     */
+    public int GetEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
