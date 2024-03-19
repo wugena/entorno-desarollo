347feab98b091897dae6b8af6e397b3dfa8876c5
wugena <162334776+wugena@users.noreply.github.com>
2024-03-19 16:20:10 +0100

    Class Figura

---
+++ Figura/src/Modelo/Rectangulo.java
@@ -0,0 +1,97 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Rectangulo {
+
+    /**
+     * Default constructor
+     */
+    public Rectangulo() {
+    }
+
+    /**
+     * 
+     */
+    private int base;
+
+    /**
+     * 
+     */
+    private int  altura;
+
+    /**
+     * 
+     */
+    public void Attribute1;
+
+    /**
+     * @param Base 
+     * @param altura
+     */
+    public void Rectangulo(int Base, int altura) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Rectangulo() {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica base de la personas
+     * @param base
+     */
+    public void Setbase(int base) {
+        // TODO implement here
+    }
+
+    /**
+     * devuelve la base de rectangulo
+     * @return
+     */
+    public int GetBase() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * Modifica Altura de la personas
+     * @param Altura
+     */
+    public void SetAltura(int Altura) {
+        // TODO implement here
+    }
+
+    /**
+     * devuelve la Altura de rectangulo
+     * @return
+     */
+    public int GetAltura() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * base * altura
+     * @return
+     */
+    public int  getarea() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 2base*2altura
+     * @return
+     */
+    public int getperimetro() {
+        // TODO implement here
+        return 0;
+    }
+
+}
